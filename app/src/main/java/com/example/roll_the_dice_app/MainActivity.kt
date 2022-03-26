101c95872d9ebea8230f95fef6b964ed91311855
kototo522 <huyejiben@gmail.com>
2022-03-26 17:23:11 +0900

    add: レイアウトの作成

--- app/src/main/java/com/example/roll_the_dice_app/MainActivity.kt
+++ app/src/main/java/com/example/roll_the_dice_app/MainActivity.kt
@@ -2,24 +2,12 @@ package com.example.roll_the_dice_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
-
-fun main() {
-    val myFirstDice = Dice(6)
-    println("Your ${myFirstDice.numSides} sided dice rolled ${myFirstDice.roll()}!")
-
-    val mySecondDice = Dice(20)
-    println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
-}
-
-class Dice (val numSides: Int) {
-    fun roll(): Int {
-        return (1..numSides).random()
     }
 }
