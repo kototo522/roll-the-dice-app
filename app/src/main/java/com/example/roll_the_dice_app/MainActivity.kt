6268b0ed8ae3a4cde7f47605c87e6475c8d0b319
kototo522 <huyejiben@gmail.com>
2022-03-26 17:40:41 +0900

    add: ボタンをインタラクティブにした

--- app/src/main/java/com/example/roll_the_dice_app/MainActivity.kt
+++ app/src/main/java/com/example/roll_the_dice_app/MainActivity.kt
@@ -6,8 +6,16 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            val resultTextView: TextView = findViewById(R.id.textView)
+            resultTextView.text = "6"
+        }
     }
 }
+
