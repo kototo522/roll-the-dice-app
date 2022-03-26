0ecf7bd85f304d63ded37244ecaa47c3e5ad5aaf
kototo522 <huyejiben@gmail.com>
2022-03-26 19:35:35 +0900

    add: 画像の追加

--- app/src/main/java/com/example/roll_the_dice_app/MainActivity.kt
+++ app/src/main/java/com/example/roll_the_dice_app/MainActivity.kt
@@ -3,7 +3,14 @@ package com.example.roll_the_dice_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,23 +19,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
-        }
+        rollButton.setOnClickListener { rollDice() }
+        rollDice()
     }
 
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
-    }
-}
-
-class Dice(private val numSides: Int) {
-
-    fun roll(): Int {
-        return (1..numSides).random()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
